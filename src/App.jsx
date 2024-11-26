57838ab3f9866dfeb381f36e85e72387e880b6f5
Gean2003 <i202114701@cibertec.edu.pe>
2024-11-26 15:29:04 -0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
-export default function () {
-  return (
-    <div>
-      <p className='text-red-700'>hola grupo</p>
-    </div>
+export default function() {
+  return ( <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/sign-in' element={<SignIn />} />
+    <Route path='/sign-up' element={<SignUp />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/profile' element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
   )
 }
